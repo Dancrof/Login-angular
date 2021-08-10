71b0e90346addc327106566c88c68fcc93c67c79
Dancrof <bryan.caicedo@exodosoft.com>
2021-08-09 20:12:08 -0500

    mejorando el responsin

--- src/app/navegation/navbar/navbar.component.ts
+++ src/app/navegation/navbar/navbar.component.ts
@@ -7,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  //[ngClass]="{'active_Menu': hideMenu == false}
+  //(click)="hideMenu = !hideMenu"
+
   hideMenu = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  /*deployCategory(){
+    let arrow = document.querySelectorAll(".arrow");
+    for (let i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+      let arrowParent = e.target.parentElement.parentElement;
+      arrowParent.classList.toggle("showMenu");
+      });
+    }
+  }*/
 }
